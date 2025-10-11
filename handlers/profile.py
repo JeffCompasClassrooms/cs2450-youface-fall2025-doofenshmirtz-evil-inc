6048d16c473bda4306587a484b24022b01fe55db
root <root@DevsLaptop.localdomain>
2025-10-11 14:52:37 -0600

    Profile page updated

--- handlers/profile.py
+++ handlers/profile.py
@@ -1,10 +1,7 @@
 import flask
 from handlers import copy
-from db import posts, users, helpers
+from db import users, helpers
 
-# -----------------------
-# PROFILE BLUEPRINT
-# -----------------------
 blueprint = flask.Blueprint("profile", __name__)
 
 @blueprint.route('/profilescreen')
@@ -12,10 +9,24 @@ def profilescreen():
     """Present a formatted user profile screen."""
     db = helpers.load_db()
 
-    # First check if already logged in
     username = flask.request.cookies.get('username')
     password = flask.request.cookies.get('password')
-    birthday = flask.request.cookies.get('birthday')
 
-    return flask.render_template('profile.html', title=copy.title,
-                                 subtitle=copy.subtitle, user=username)
+    if not username or not password:
+        return flask.redirect(flask.url_for('login.index'))
+
+    user = users.get_user(db, username, password)
+    if not user:
+        return flask.redirect(flask.url_for('login.index'))
+
+    birthday = user.get('birthday')
+    age = users.calculate_age(birthday) if birthday else None
+
+    return flask.render_template(
+        'profile.html',
+        title=copy.title,
+        subtitle=copy.subtitle,
+        user=username,
+        birthday=birthday,
+        age=age
+    )
