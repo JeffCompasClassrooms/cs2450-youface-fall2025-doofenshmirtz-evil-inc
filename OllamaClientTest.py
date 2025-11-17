c78537c227269ab543450a8f18d75c565f497d3a
d00525485 <d00525485@utahtech.edu>
2025-11-17 10:25:47 -0700

    Ollama bug fixes, added 'API key'

--- OllamaClientTest.py
+++ OllamaClientTest.py
@@ -2,7 +2,7 @@ import requests
 
 PROMPT = "whats 2+2, and tell me a quick knock knock joke. KEEP IT SHORT"
 resp = requests.post(
-    "http://127.0.0.1:5007/generate",
+    "http://theotherone.dev:5007/generate",
     headers={"X-API-Key": ")@W^utYCh:2A|RR(bT%_0!nhvLjP{tD|L:Lo.x*P:ouncm[G'5=w]yMpEPJ@c7D"},
     json={"prompt": PROMPT}
 )
